1b8eb800fd3bfa68e21fef1c97f03aa92b2e9675
Harold <haragon@espol.edu.ec>
2018-10-15 12:56:20 -0500

    Method to change .csv files to .tfrecords to improve performance and test

---
+++ model.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+import pandas as pd
+import os
+
+#This function transform csv files to tfrecords, to improve performance of algoritms.
+#To replace files send 1 as parameter
+def csvToTfRecords(replace=0, csv_dir="traces_csv", tfr_dir="traces_tfrecords"):
+	traces_csv=os.listdir(csv_dir)
+	for trace_csv in traces_csv:
+		csv_file=pd.read_csv(csv_dir+"/"+trace_csv).values
+		with tf.python_io.TFRecordWriter(tfr_dir+"/"+trace_csv.split(".")[0]+".tfrecords") as writer:
+			for row in csv_file:
+				#inputs,label
+				# TODO(H): Check if indexs match with csv files
+				features, labels = row[:-1], row[-1] 
+				ex = tf.train.Example()
+				# TODO(H): Ask for correct datatypes and change them
+				ex.features.feature["features"].float_list.value.extend(features)
+				ex.features.feature["labels"].float_list.value.append(labels) 
+				writer.write(ex.SerializeToString())
+
