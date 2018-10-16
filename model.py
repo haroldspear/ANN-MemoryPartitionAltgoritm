033d476fe331cc2215c9d94904cc1b077058ef29
Harold <haragon@espol.edu.ec>
2018-10-16 13:46:39 -0500

    parse_fn function defined

--- model.py
+++ model.py
@@ -18,11 +18,27 @@ def csvToTfRecords(replace=0, csv_dir="traces_csv", tfr_dir="traces_tfrecords"):
 			csv_file=pd.read_csv(csv_dir+"/"+trace_csv).values
 			with tf.python_io.TFRecordWriter(tfr_dir+"/"+trace_csv.split(".")[0]+".tfrecords") as writer:
 				for row in csv_file:
-					#inputs,label
+					#inputs,labels
 					# TODO(H): Check if indexs match with csv files
-					features, labels = row[:-1], row[-1] 
+					inputs, labels = row[:-4], row[-4] 
 					ex = tf.train.Example()
 					# TODO(H): Ask for correct datatypes and change them
-					ex.features.feature["features"].float_list.value.extend(features)
-					ex.features.feature["labels"].float_list.value.append(labels) 
-					writer.write(ex.SerializeToString())		
+					ex.features.feature["inputs"].float_list.value.extend(inputs)
+					ex.features.feature["labels"].float_list.value.extend(labels) 
+					writer.write(ex.SerializeToString())
+
+def parse_fn(record):
+	#Transform string data to features
+	# TODO(H): Change datatypes if is necessary when test with real dataset, and using tf.cast or tf.convert_to_tensor
+    features={
+      'inputs': tf.FixedLenFeature([], tf.float),
+      'labels': tf.FixedLenFeature([], tf.float),
+    }
+
+    parsed = tf.parse_single_example(record, features)
+
+    inputs = tf.cast(parsed['inputs'], float64)
+    labels = tf.cast(parsed['labels'], float64)
+
+    return {'inputs': inputs}, {'labels': labels}
+
